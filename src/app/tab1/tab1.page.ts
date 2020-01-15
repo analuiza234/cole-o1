f15affdbb26478a087fac17befdc32b3b0564d84
Técnico em Informática <24121742018.1@senacrjedu.local>
2020-01-15 19:30:18 -0300

    ana

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,22 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { Usuario } from '../model/usuario';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { UsuarioService } from '../services/usuario.service';
+import { Usuario } from '../model/usuario';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: './tab1.page.html',
-  styleUrls: ['./tab1.page.scss'],
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit {
+export class Tab1Page {
+
   protected usuario: Usuario = new Usuario;
   protected id: string = null;
 
+constructor(
+protected activatedRoute:ActivatedRoute,
+protected usuarioService: UsuarioService,)
+{}
 
-  constructor(
-    protected UsuarioService: UsuarioService) {
-   }
-
-  ngOnInit() {
+ngOnInit() {
+  this.id = this.activatedRoute.snapshot.paramMap.get("id");
+  if (this.id) {
+    this.usuarioService.get(this.id).subscribe(
+      res => {
+        this.usuario = res
+      },
+      erro => this.id = null
+    )
   }
+}
 
 }
